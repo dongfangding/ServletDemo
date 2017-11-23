658eda20866b7bf467970f77a179cf04ea04b510
dongfang.ding <1041765757@qq.com>
2017-11-23 23:19:12 +0800

    登陆拦截忽略URL

--- src/training/filter/LoginFilter.java
+++ src/training/filter/LoginFilter.java
@@ -1,6 +1,8 @@
 package training.filter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -8,6 +10,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import module.user.entity.User;
+
+
+/**
+ * 对每个请求进行登陆拦截验证
+ * @author Administrator
+ *
+ */
 public class LoginFilter extends HttpFilter {
 
 	@Override
@@ -16,20 +26,40 @@ public class LoginFilter extends HttpFilter {
 		System.out.println("LoginFilter working...........");
 		HttpSession session = request.getSession();
 		String servletPath = request.getServletPath();
-		if ("/codeGenerateServlet.do".equals(servletPath)) {
-			filterChain.doFilter(request, response);
-			return;
+		System.out.println(servletPath);
+		// 忽略的url验证列表
+		String exIncludeUrl = super.getFilterConfig().getInitParameter("excludeUrls");
+		if (exIncludeUrl != null && !"".equals(exIncludeUrl)) {
+			System.out.println("忽略验证登陆的Url为： " + exIncludeUrl);
+			List<String> excludeUrlList = Arrays.asList(exIncludeUrl.split(","));
+			if (excludeUrlList != null && excludeUrlList.size() > 0) {
+				for (String url : excludeUrlList) {
+					if (url.contains("/*")) {
+						if (servletPath.startsWith(url.substring(0, url.length() - 2))) {
+							filterChain.doFilter(request, response);
+							return;
+						}
+					}
+					if (url.equals(servletPath)) {
+						filterChain.doFilter(request, response);
+						return;
+					}
+				}
+			}
+		} else {
+			throw new RuntimeException("忽略验证的url为空，当前会出现死循环！");
 		}
 		if (session.getAttribute("user") == null) {
 			try {
-				request.getRequestDispatcher("/userLoginJspServlet.do").forward(request, response);
+				request.getRequestDispatcher("/moduleUserLoginJspServlet.do").forward(request, response);
 			} catch (ServletException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		} else {
+			filterChain.doFilter(request, response);
 		}
-		filterChain.doFilter(request, response);
 	}
 	
 }
